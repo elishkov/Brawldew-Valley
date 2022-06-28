b4fc5a5a79c15f2a045a6d80c5103970ea52e5bf
Alon <elishkov@gmail.com>
2022-06-28 09:28:17 +0300

    fixed floating text layer and position

--- Assets/scripts/Character.cs
+++ Assets/scripts/Character.cs
@@ -9,6 +9,8 @@ public class Character : MonoBehaviour
     [SerializeField] public long max_health = 100;
     [SerializeField] Text cur_health_txt;
     [SerializeField] Text max_health_txt;
+    [SerializeField] float horizontal_floating_txt_scatter = 0.2f;
+
     private bool is_dead = false;
     
     private Animator animator;
@@ -34,7 +36,23 @@ public class Character : MonoBehaviour
     public void TakeDamage(long amount)
     {
         animator.SetTrigger("Hurt");
-        GameManager.instance.onScreenMessageSystem.PostMessage(transform.position, amount.ToString());
+
+        
+        // scatter x axis
+        var text_pos_x = Random.Range(
+            transform.position.x - horizontal_floating_txt_scatter,
+            transform.position.x + horizontal_floating_txt_scatter
+            );
+
+        // raise above character
+        var text_pos_y = transform.position.y + transform.localScale.y;
+
+
+        var textPosition = new Vector3(
+            text_pos_x,
+            text_pos_y
+            );
+        GameManager.instance.onScreenMessageSystem.PostMessage(textPosition, amount.ToString());
 
         cur_health = (long)Mathf.Max(0, cur_health - amount);
         
